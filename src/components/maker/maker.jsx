e37ca52d1ba7d1e7d043e9f967c41aff261f5e96
nscoo <nscoo@naver.com>
2022-06-21 14:36:10 +0900

    add editor, preview layout

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,27 +1,69 @@
 import React, { useEffect } from 'react';
+import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import Editor from '../editor/editor';
 import Footer from '../footer/footer';
 import Header from '../header/header';
+import Preview from '../preview/preview';
 import styles from './maker.module.css'
-const Maker = ({authService}) => {
+const Maker = ({ authService }) => {
+    const [cards, setCards] = useState([
+        {
+            id: '1',
+            name: 'SangCheol1111',
+            company: 'SungeunENG',
+            theme: 'dark',
+            title: 'Developer',
+            email: 'nscoo@naver.com',
+            message: 'holy moly',
+            fileName: 'Roh',
+            fileURL: 'Roh.png',
+        },
+        {
+            id: '2',
+            name: 'SangCheol2',
+            company: 'SungeunENG',
+            theme: 'light',
+            title: 'Developer',
+            email: 'nscoo@naver.com',
+            message: 'holy moly',
+            fileName: 'Roh',
+            fileURL: null,
+        },
+        {
+            id: '3',
+            name: 'SangCheol3',
+            company: 'SungeunENG',
+            theme: 'colorful',
+            title: 'Developer',
+            email: 'nscoo@naver.com',
+            message: 'holy moly',
+            fileName: 'Roh',
+            fileURL: null,
+        }
+    ])
     const history = useHistory();
     const onLogout = () => {
         authService.logout();
     };
     useEffect(() => {
-        authService.onAuthChange(user=>{
-            if(!user){
+        authService.onAuthChange(user => {
+            if (!user) {
                 history.push('/');
             }
         });
     })
     return (
         <section className={styles.maker}>
-            <Header onLogout={onLogout}/>
-            <Footer/>
+            <Header onLogout={onLogout} />
+            <div className={styles.container}>
+                <Editor cards={cards} />
+                <Preview cards={cards} />
+            </div>
+            <Footer />
         </section>
     )
 }
-    
+
 
 export default Maker;
