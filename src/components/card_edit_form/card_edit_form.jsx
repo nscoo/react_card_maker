68ff259acd41e70bf18616b67accf4395f782242
nscoo <nscoo@naver.com>
2022-06-23 21:34:24 +0900

    connect to firebase

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -1,96 +1,98 @@
-import React from 'react';
-import { useRef } from 'react';
+import React, { useRef } from 'react';
+import styles from './card_edit_form.module.css';
 import Button from '../button/button';
 
-import styles from './card_edit_form.module.css'
-const CardEditForm = ({FileInput, card, updateCard, deleteCard }) => {
-    const nameRef = useRef();
-    const companyRef = useRef();
-    const themeRef = useRef();
-    const titleRef = useRef();
-    const emailRef = useRef();
-    const messageRef = useRef();
+const CardEditForm = ({ FileInput, card, updateCard, deleteCard }) => {
+  const nameRef = useRef();
+  const companyRef = useRef();
+  const themeRef = useRef();
+  const titleRef = useRef();
+  const emailRef = useRef();
+  const messageRef = useRef();
 
-    const onFileChange= (file)=>{
-        updateCard({
-            ...card,
-            fileName:file.name,
-            fileURL:file.url,
-        });
-    }
+  const { name, company, title, email, message, theme, fileName } = card;
 
-    const { name, company, title, email, message, theme, fileName } = card;
-    const onChange = (event) =>{
-        if(event.currentTarget == null){
-            return;
-        }
-        event.preventDefault();
-        updateCard({
-            ...card,
-            [event.currentTarget.name] : event.currentTarget.value,
-        })
+  const onFileChange = file => {
+    updateCard({
+      ...card,
+      fileName: file.name,
+      fileURL: file.url,
+    });
+  };
+
+  const onChange = event => {
+    if (event.currentTarget == null) {
+      return;
     }
-    const onSubmit = () => {
-        deleteCard(card);
-    };
-    return (
-        <form className={styles.form}>
-            <input 
-            className={styles.input}
-             type="text" 
-             name='name'
-             ref={nameRef} 
-             value={name} 
-             onChange={onChange}
-             />
-            <input 
-            className={styles.input}
-            type="text" 
-            name='company'
-            ref={companyRef} 
-            value={company}
-            onChange={onChange}
-            />
-            <select 
-            className={styles.select} 
-            name='theme'
-            ref={themeRef} 
-            value={theme}
-            onChange={onChange}
-            >
-                <option value="light">light</option>
-                <option value="dark">dark</option>
-                <option value="colorful">colorful</option>
-            </select>
-            <input 
-            className={styles.input} 
-            type="text" 
-            name='title'
-            ref={titleRef} 
-            value={title} 
-            onChange={onChange}
-            />
-            <input 
-            className={styles.input} 
-            type="text" 
-            name='email'
-            ref={emailRef}
-            value={email} 
-            onChange={onChange}
-            />
-            <textarea 
-            className={styles.textarea} 
-            name="message"
-            ref={messageRef} 
-            value={message}
-            onChange={onChange}
-            />
-            <div className={styles.fileInput}>
-            <FileInput name={fileName} onFileChange={onFileChange}/>
-            </div>
-            <Button name='Delete' onClick={onSubmit}/>
-        </form>
-    );
+    event.preventDefault();
+    updateCard({
+      ...card,
+      [event.currentTarget.name]: event.currentTarget.value,
+    });
+  };
+
+  const onSubmit = () => {
+    deleteCard(card);
+  };
+
+  return (
+    <form className={styles.form}>
+      <input
+        className={styles.input}
+        type="text"
+        name="name"
+        ref={nameRef}
+        value={name}
+        onChange={onChange}
+      />
+      <input
+        className={styles.input}
+        type="text"
+        name="company"
+        ref={companyRef}
+        value={company}
+        onChange={onChange}
+      />
+      <select
+        className={styles.select}
+        name="theme"
+        ref={themeRef}
+        value={theme}
+        onChange={onChange}
+      >
+        <option value="light">light</option>
+        <option value="dark">dark</option>
+        <option value="colorful">colorful</option>
+      </select>
+      <input
+        className={styles.input}
+        type="text"
+        name="title"
+        ref={titleRef}
+        value={title}
+        onChange={onChange}
+      />
+      <input
+        className={styles.input}
+        type="text"
+        name="email"
+        ref={emailRef}
+        value={email}
+        onChange={onChange}
+      />
+      <textarea
+        className={styles.textarea}
+        ref={messageRef}
+        name="message"
+        value={message}
+        onChange={onChange}
+      />
+      <div className={styles.fileInput}>
+        <FileInput name={fileName} onFileChange={onFileChange} />
+      </div>
+      <Button name="Delete" onClick={onSubmit} />
+    </form>
+  );
 };
 
 export default CardEditForm;
--- src/components/image_file_input/image_file_input.jsx
+++ src/components/image_file_input/image_file_input.jsx
@@ -1,7 +1,9 @@
-import React, { useRef,useState } from 'react';
+import React, { useRef, useState } from 'react';
 import styles from './image_file_input.module.css';
+
 const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
-  const[loading,setLoading]=useState(false);
+  const [loading, setLoading] = useState(false);
+
   const inputRef = useRef();
   const onButtonClick = event => {
     event.preventDefault();
@@ -28,15 +30,15 @@ const ImageFileInput = ({ imageUploader, name, onFileChange }) => {
         name="file"
         onChange={onChange}
       />
-      {
-        !loading &&
-        <button className={`${styles.button} ${name ? styles.pink : styles.grey}`} onClick={onButtonClick}>
-        {name || 'No file'}
-      </button>
-      }
-      {
-        loading &&<div className={styles.loading}></div>
-      }
+      {!loading && (
+        <button
+          className={`${styles.button} ${name ? styles.pink : styles.grey}`}
+          onClick={onButtonClick}
+        >
+          {name || 'No file'}
+        </button>
+      )}
+      {loading && <div className={styles.loading}></div>}
     </div>
   );
 };
